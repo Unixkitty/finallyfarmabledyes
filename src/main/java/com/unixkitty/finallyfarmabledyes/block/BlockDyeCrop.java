30ef694e84432ba78ef2bdd0dd68de5f5d4dbfac
Unixkitty <uni@unixkitty.com>
2022-01-07 16:40:24 +0300

    Added remaining four small flowers

--- src/main/java/com/unixkitty/finallyfarmabledyes/block/BlockDyeCrop.java
+++ src/main/java/com/unixkitty/finallyfarmabledyes/block/BlockDyeCrop.java
@@ -5,7 +5,6 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.BeetrootBlock;
@@ -19,24 +18,18 @@ import java.util.Objects;
 
 public class BlockDyeCrop extends BeetrootBlock
 {
-    private final DyeColor color;
-    //    private final LazyLoadedValue<ItemLike> seeds;
+    private final ItemLike flowerItem;
     private final NonNullLazy<ItemLike> seeds;
 
-    public BlockDyeCrop(DyeColor color)
+    public BlockDyeCrop(ItemLike flowerItem, DyeColor color)
     {
         super(Block.Properties.copy(Blocks.BEETROOTS));
 
-        this.color = color;
+        this.flowerItem = flowerItem;
         this.seeds = NonNullLazy.of(() ->
                 Objects.requireNonNull(ForgeRegistries.ITEMS.getValue(new ResourceLocation(FinallyFarmableDyes.MODID, color.toString())))
         );
     }
-
-/*    public DyeColor getColor()
-    {
-        return this.color;
-    }*/
 
     //Seed
     @Override
@@ -52,21 +45,8 @@ public class BlockDyeCrop extends BeetrootBlock
         return new ItemStack(getFlowerItem());
     }
 
-
     public ItemLike getFlowerItem()
     {
-        return switch (this.color)
-                {
-                    case WHITE -> Items.LILY_OF_THE_VALLEY;
-                    case ORANGE -> Items.ORANGE_TULIP;
-                    case MAGENTA -> Items.ALLIUM;
-                    case LIGHT_BLUE -> Items.BLUE_ORCHID;
-                    case YELLOW -> Items.DANDELION;
-                    case PINK -> Items.PINK_TULIP;
-                    case LIGHT_GRAY -> Items.WHITE_TULIP;
-                    case BLUE -> Items.CORNFLOWER;
-                    case BLACK -> Items.WITHER_ROSE;
-                    default -> Items.BEEF;
-                };
+        return this.flowerItem;
     }
 }
