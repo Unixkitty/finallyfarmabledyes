9ed9690ecbe16a43dc6a024560764709da545718
Unixkitty <uni@unixkitty.com>
2023-04-06 16:53:39 +0400

    Updated to 1.19.2

--- src/main/java/com/unixkitty/finallyfarmabledyes/FinallyFarmableDyes.java
+++ src/main/java/com/unixkitty/finallyfarmabledyes/FinallyFarmableDyes.java
@@ -1,13 +1,23 @@
 package com.unixkitty.finallyfarmabledyes;
 
-import com.unixkitty.finallyfarmabledyes.init.ModBlocks;
 import com.unixkitty.finallyfarmabledyes.init.ModRegistry;
+import net.minecraft.world.entity.animal.Chicken;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.level.block.ComposterBlock;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.RegistryObject;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Mod(FinallyFarmableDyes.MODID)
 public class FinallyFarmableDyes
@@ -22,9 +32,24 @@ public class FinallyFarmableDyes
     {
         final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        MinecraftForge.EVENT_BUS.register(ModRegistry.class);
+        ModRegistry.BLOCKS.register(modEventBus);
+        ModRegistry.ITEMS.register(modEventBus);
 
-        ModBlocks.BLOCKS.register(modEventBus);
+        MinecraftForge.EVENT_BUS.addListener(FinallyFarmableDyes::commonSetup);
+    }
+
+    public static void commonSetup(final FMLCommonSetupEvent event)
+    {
+        final List<ItemStack> chickenFoodList = new ArrayList<>(Arrays.stream(Chicken.FOOD_ITEMS.getItems()).toList());
+
+        ModRegistry.ITEMS.getEntries().stream().map(RegistryObject::get).forEach(item ->
+        {
+            ComposterBlock.COMPOSTABLES.putIfAbsent(item, ComposterBlock.COMPOSTABLES.getOrDefault(Items.WHEAT_SEEDS, 0.3f));
+
+            chickenFoodList.add(new ItemStack(item));
+        });
+
+        Chicken.FOOD_ITEMS = Ingredient.of(chickenFoodList.stream());
     }
 
     public static Logger log()
--- src/main/java/com/unixkitty/finallyfarmabledyes/init/ModRegistry.java
+++ src/main/java/com/unixkitty/finallyfarmabledyes/init/ModRegistry.java
@@ -1,43 +1,65 @@
 package com.unixkitty.finallyfarmabledyes.init;
 
 import com.unixkitty.finallyfarmabledyes.FinallyFarmableDyes;
-import net.minecraft.world.entity.animal.Chicken;
+import com.unixkitty.finallyfarmabledyes.block.BlockDyeCrop;
 import net.minecraft.world.item.*;
-import net.minecraft.world.item.crafting.Ingredient;
-import net.minecraft.world.level.block.ComposterBlock;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
+import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
-
 @SuppressWarnings("unused")
-@Mod.EventBusSubscriber(modid = FinallyFarmableDyes.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public final class ModRegistry
 {
-//    private static final List<ItemStack> chickenFoodList = new ArrayList<>();
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, FinallyFarmableDyes.MODID);
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, FinallyFarmableDyes.MODID);
 
-    @SubscribeEvent
-    public static void onRegisterItems(final RegistryEvent.Register<Item> event)
+    /*
+            WHITE           Lily of the Valley
+            ORANGE          Orange Tulip
+            MAGENTA         Allium
+            LIGHT_BLUE      Blue Orchid
+            YELLOW          Dandelion
+            PINK            Pink Tulip
+            LIGHT_GRAY      White Tulip
+            BLUE            Cornflower
+            RED             Poppy
+            BLACK           Wither Rose
+         */
+
+    public static final RegistryObject<Block> WHITE = registerCrop(Items.LILY_OF_THE_VALLEY, DyeColor.WHITE);
+    public static final RegistryObject<Block> ORANGE = registerCrop(Items.ORANGE_TULIP, DyeColor.ORANGE);
+    public static final RegistryObject<Block> MAGENTA = registerCrop(Items.ALLIUM, DyeColor.MAGENTA);
+    public static final RegistryObject<Block> LIGHT_BLUE = registerCrop(Items.BLUE_ORCHID, DyeColor.LIGHT_BLUE);
+    public static final RegistryObject<Block> YELLOW = registerCrop(Items.DANDELION, DyeColor.YELLOW);
+    public static final RegistryObject<Block> PINK = registerCrop(Items.PINK_TULIP, DyeColor.PINK);
+    public static final RegistryObject<Block> LIGHT_GRAY = registerCrop(Items.WHITE_TULIP, DyeColor.LIGHT_GRAY);
+    public static final RegistryObject<Block> BLUE = registerCrop(Items.CORNFLOWER, DyeColor.BLUE);
+    public static final RegistryObject<Block> RED = registerCrop(Items.POPPY, DyeColor.RED);
+    public static final RegistryObject<Block> BLACK = registerCrop(Items.WITHER_ROSE, DyeColor.BLACK);
+
+    //Extra
+    public static final RegistryObject<Block> RED_TULIP = registerExtraCrop(Items.RED_TULIP, DyeColor.RED);
+    public static final RegistryObject<Block> AZURE_BLUET = registerExtraCrop(Items.AZURE_BLUET, DyeColor.LIGHT_GRAY);
+    public static final RegistryObject<Block> OXEYE_DAISY = registerExtraCrop(Items.OXEYE_DAISY, DyeColor.LIGHT_GRAY);
+
+    private static RegistryObject<Block> registerCrop(ItemLike flowerItem, DyeColor color)
     {
-        final List<ItemStack> chickenFoodList = new ArrayList<>(Arrays.stream(Chicken.FOOD_ITEMS.getItems()).toList());
+        return register(color.toString(), flowerItem, color);
+    }
 
-        // BlockItem seeds for all blocks
-        ModBlocks.BLOCKS.getEntries().stream().map(RegistryObject::get).forEach(block ->
-        {
-            Item item = new ItemNameBlockItem(block, new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)).setRegistryName(Objects.requireNonNull(block.getRegistryName()));
+    private static RegistryObject<Block> registerExtraCrop(ItemLike flowerItem, DyeColor color)
+    {
+        return register(ForgeRegistries.ITEMS.getKey(flowerItem.asItem()).getPath(), flowerItem, color);
+    }
 
-            event.getRegistry().register(item);
+    private static RegistryObject<Block> register(final String name, ItemLike flowerItem, DyeColor color)
+    {
+        RegistryObject<Block> block = BLOCKS.register(name, () -> new BlockDyeCrop(flowerItem, color));
 
-            ComposterBlock.COMPOSTABLES.putIfAbsent(item, ComposterBlock.COMPOSTABLES.getOrDefault(Items.WHEAT_SEEDS, 0.3f));
+        ITEMS.register(name, () -> new ItemNameBlockItem(block.get(), new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)));
 
-            chickenFoodList.add(new ItemStack(item));
-        });
-
-        Chicken.FOOD_ITEMS = Ingredient.of(chickenFoodList.stream());
+        return block;
     }
 }
